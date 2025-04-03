2fb5c18874eeef92025a22057f7cfa8bab6382f3
prame BT <pramebt@gmail.com>
2025-04-03 19:23:05 +0700

    first commit

---
+++ src/components/AgeCal.tsx
@@ -0,0 +1,32 @@
+const AgeCal = () => {
+  return (
+    <div className="bg-white w-[800px] h-[600px] rounded-4xl rounded-br-[150px]">
+      <div className="flex gap-[30px] mt-[90px] ml-[58px]">
+        <div>
+          <h1>D A Y S</h1>
+          <input type="" className="border w-[150px] h-[60px]" />
+        </div>
+        <div>
+          <h1>D A Y S</h1>
+          <input type="" className="border w-[150px] h-[60px]" />
+        </div>
+        <div>
+          <h1>D A Y S</h1>
+          <input type="" className="border w-[150px] h-[60px]" />
+        </div>
+      </div>
+
+      <div className="h-[90px]  flex items-center ">
+        <div className="bg-gray-400 w-[600px] h-[1px] ml-[58px]"></div>
+        <div className="bg-black w-[90px] h-[90px] rounded-full mr-[66px] absolute right-[70px] "></div>
+      </div>
+
+      <div className="">
+      <h1><span className="border  text-[96px] h-fit">333333</span></h1>
+      <h1><span className="text-[96px]">333333</span></h1>
+        <h1 className="text-[96px]">333333</h1>
+      </div>
+    </div>
+  );
+};
+export default AgeCal;
